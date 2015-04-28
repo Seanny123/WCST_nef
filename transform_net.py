5cd5dd0e0407f7d3d9aa56c59d8cd41886ac14eb
Sean Aubin <saubin@uwaterloo.ca>
2015-04-27 23:29:15 -0400

    mem works

--- transform_net.py
+++ transform_net.py
@@ -4,6 +4,7 @@ from integrator import ea_integrator
 # how to efficiently pass parameters?
 def transform_net():
 	with nengo.Network(label="transform") as t_n:
-		t_n.input = nengo.networks.workingmemory()
+		t_n.mem_input = nengo.networks.workingmemory.InputGatedMemory()
 		t_n.transform = ea_integrator()
+		nengo.Connection(t_n.mem_input.output, t_n.transform.input)
 	return t_n
